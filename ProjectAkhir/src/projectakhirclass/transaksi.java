f60858e709b045a2b30653099466489953602bc0
Bahrudin <BahrudinRizky313@gmail.com>
2018-12-06 20:01:42 +0700

    membuat frame login

--- ProjectAkhir/src/projectakhirclass/transaksi.java
+++ ProjectAkhir/src/projectakhirclass/transaksi.java
@@ -5,10 +5,20 @@
  */
 package projectakhirclass;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Bahrudin<bahrudinrizky313@gmai.com>
  */
 public class transaksi {
-    
+    private String nama;
+    private String alamat;
+    private int noHp;
+    private int ink;
+    private ArrayList<Merk> merk = new ArrayList<>();
+    private String tipe;
+    private int nopol;
+    private String warna;
+    private String jt;
 }
